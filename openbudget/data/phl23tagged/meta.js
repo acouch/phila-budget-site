015cdd80c3cf62bc3b0c58cf7cb60ac19e551da6
acouch <acouch@users.noreply.github.com>
2026-05-15 16:41:47 +0000

    deploy: 7651367571f6360b728be678b0366c8cb3cead06

---
+++ openbudget/data/phl23tagged/meta.js
@@ -0,0 +1,28 @@
+window.OpenBudget = {"data":{"meta":{
+    "hierarchy": ["Funding category", "Category", "Fund"],
+    "page_title": "City of Philadelphia Budget",
+    "description": "",
+    "h1": "City of Philadelphia",
+    "h2": "Budget by category",
+    "data_link": "https://github.com/CityOfPhiladelphia/open-budget-data-transformer/blob/master/data/FY2014-actual.csv",
+    "data_title": "Download Data",
+    "uservoice": "",
+    "base_headline": "Funding categories",
+    "gross_cost_label": "Expenses",
+    "revenue_label": "Revenues",
+    "currency_prefix": "$",
+    "overview_label": "Overview",
+    "deficit_label": "Deficit",
+    "surplus_label": "Surplus",
+    "data_url": "data/phltagged/data.json",
+    "value": {
+        "label": "FY 2023",
+        "type": "accounts",
+        "year": "2023"
+    },
+    "value2": {
+        "label": "FY 2022",
+        "type": "accounts",
+        "year": "2022"
+    }
+}}};
